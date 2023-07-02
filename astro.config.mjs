adae8519c3f83ee364ec4733fd5568bba36174b1
JohnDeveloper <johnisthedeveloper@gmail.com>
2023-07-02 02:00:17 -0400

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), mdx()],
+  integrations: [sitemap(), mdx(), tailwind()],
   site: "https://astro-theme-creek.netlify.app/"
 });
